59ccf4c2a4ce7247bb168087b90c1f7e80b79a49
Simon Robertson <simon.robertson@shift.online>
2024-08-14 08:53:46 +0100

    Add the Roboto fonts

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -1,3 +1,8 @@
 export function Application() {
-    return <main>Hello</main>
+    return (
+        <main>
+            <p>Coming soon&trade;</p>
+            <p>I'm working on it ...</p>
+        </main>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import { Application } from "app/components/application"
 
 import { render } from "solid-js/web"
 
+import "./index.css"
+
 render(Application, document.body)
