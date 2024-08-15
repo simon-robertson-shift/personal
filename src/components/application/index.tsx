19721aad57e9dea94127b5c076337684912baa9b
Simon Robertson <simon.robertson@shift.online>
2024-08-15 14:18:07 +0100

    Add placeholder page routes

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -9,6 +9,9 @@ import { lazy } from "solid-js"
 import css from "./index.module.css"
 
 const Home = lazy(() => import("app/components/pages/home"))
+const JitTranslations = lazy(() => import("app/components/pages/jit-translations"))
+const SmartFlows = lazy(() => import("app/components/pages/smart-flows"))
+const SmartSupport = lazy(() => import("app/components/pages/smart-support"))
 
 export function Application() {
     return (
@@ -17,6 +20,9 @@ export function Application() {
             <MetaProvider>
                 <Router>
                     <Route path="/" component={Home} />
+                    <Route path="/jit-translations" component={JitTranslations} />
+                    <Route path="/smart-flows" component={SmartFlows} />
+                    <Route path="/smart-support" component={SmartSupport} />
                 </Router>
             </MetaProvider>
             <PageFooter />
