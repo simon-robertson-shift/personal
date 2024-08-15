2a3b5a8beae7123560a7305d35c57c184a7f6a8e
Simon Robertson <simon.robertson@shift.online>
2024-08-15 16:40:55 +0100

    Add a title attribute to the navigator link

--- src/components/core/page-navigator/index.tsx
+++ src/components/core/page-navigator/index.tsx
@@ -3,7 +3,9 @@ import css from "./index.module.css"
 export function PageNavigator() {
     return (
         <nav class={css.component}>
-            <a href="/">Simon Robertson</a>
+            <a href="/" title="Simon Robertson">
+                Simon Robertson
+            </a>
         </nav>
     )
 }
