7d90849d3865025bb52f52062a4a8440fc478f55
Simon Robertson <simon.robertson@shift.online>
2024-08-15 14:06:53 +0100

    Make the PageHeader component generic

--- src/components/core/page-header/index.tsx
+++ src/components/core/page-header/index.tsx
@@ -1,3 +1,31 @@
-export function PageHeader() {
-    return <header></header>
+import { Meta, Title } from "@solidjs/meta"
+
+import { Fragment } from "app/components/core/fragment"
+
+import css from "./index.module.css"
+
+type Props = {
+    readonly title: string
+    readonly description: string
+    readonly metaTitle?: string
+    readonly metaDescription?: string
+    readonly bypassMeta?: boolean
+}
+
+export function PageHeader({
+    title,
+    description,
+    metaTitle = title,
+    metaDescription = description
+}: Props) {
+    return (
+        <Fragment>
+            <Title>{metaTitle}</Title>
+            <Meta name="description" content={metaDescription} />
+            <header class={css.component}>
+                <h1>{title}</h1>
+                <em>{description}</em>
+            </header>
+        </Fragment>
+    )
 }
