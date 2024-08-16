2ef30a6982f59ebedaf529c0131b0e752e0e010c
Simon Robertson <simon.robertson@shift.online>
2024-08-16 21:37:10 +0100

    Style updates

--- src/components/page-elements/articles-element/index.tsx
+++ src/components/page-elements/articles-element/index.tsx
@@ -1,5 +1,7 @@
 import { fetchPageMetadataCollection } from "@/data/pages"
 
+import css from "./index.module.css"
+
 export async function ArticlesElement() {
     const collection = await fetchPageMetadataCollection()
 
@@ -9,13 +11,21 @@ export async function ArticlesElement() {
         }
 
         return (
-            <a key={key} href={`/${metadata.slug}`} title={metadata.title}>
-                <article>
-                    <h1>{metadata.title}</h1>
+            <a
+                className={css.link}
+                key={key}
+                href={`/${metadata.slug}`}
+                title={metadata.title}
+                style={{ backgroundColor: metadata.color }}
+            >
+                <article className={css.article}>
+                    <h1 className={css.title}>{metadata.title}</h1>
+                    <p className={css.category}>{metadata.category}</p>
+                    <p className={css.description}>{metadata.description}</p>
                 </article>
             </a>
         )
     })
 
-    return <nav>{conent}</nav>
+    return <nav className={css.navigation}>{conent}</nav>
 }
