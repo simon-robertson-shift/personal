15ac3008fb41f8d1bb6bbb140a8d76a4c7758679
Simon Robertson <simon.robertson@shift.online>
2024-08-16 18:35:02 +0100

    Update "articles" and "code" content elements

--- src/components/page-elements/articles-element/index.tsx
+++ src/components/page-elements/articles-element/index.tsx
@@ -1,3 +1,21 @@
-export function ArticlesElement() {
-    return null
+import { fetchPageMetadataCollection } from "@/data/pages"
+
+export async function ArticlesElement() {
+    const collection = await fetchPageMetadataCollection()
+
+    const conent = collection.map((metadata, key) => {
+        if (metadata.slug === "home") {
+            return null
+        }
+
+        return (
+            <a key={key} href={`/${metadata.slug}`} title={metadata.title}>
+                <article>
+                    <h1>{metadata.title}</h1>
+                </article>
+            </a>
+        )
+    })
+
+    return <nav>{conent}</nav>
 }
--- src/components/page-elements/code-element/index.tsx
+++ src/components/page-elements/code-element/index.tsx
@@ -1,3 +1,21 @@
-export function CodeElement() {
-    return null
+type Props = {
+    readonly data: {
+        readonly title: string
+        readonly lines: string[]
+    }
+}
+
+export function CodeElement({ data }: Props) {
+    const content = data.lines.map((line, key) => {
+        return <span key={key}>{line}</span>
+    })
+
+    return (
+        <aside>
+            <div>
+                <span>{data.title}</span>
+            </div>
+            <div>{content}</div>
+        </aside>
+    )
 }
