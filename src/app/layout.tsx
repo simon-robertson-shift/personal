c59e7118c6c40a8e0d846f8b503cb01f6702d6d9
Simon Robertson <simon.robertson@shift.online>
2024-08-16 19:40:19 +0100

    Add initial fonts and colour theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
+import { Noto_Sans, Roboto_Mono } from "next/font/google"
+
 import { ReactNode } from "react"
+
+import "./globals.css"
 
 type Props = {
     readonly children: ReactNode
 }
 
+const fontMain = Noto_Sans({ variable: "--font-main", weight: ["400"], subsets: ["latin"] })
+const fontMono = Roboto_Mono({ variable: "--font-mono", weight: ["400"], subsets: ["latin"] })
+
 export default function Layout({ children }: Props) {
+    const fontClassses = [fontMain.variable, fontMono.variable]
+
     return (
-        <html lang="en">
+        <html lang="en" className={fontClassses.join(" ")}>
             <body>{children}</body>
         </html>
     )
