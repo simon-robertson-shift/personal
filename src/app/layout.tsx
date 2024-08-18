b39431aa224618ebefa94dde623048299ddadb89
Simon Robertson <simon.robertson@shift.online>
2024-08-18 09:49:37 +0100

    Add viewport settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Viewport } from "next"
+
 import { Roboto_Flex, Roboto_Mono } from "next/font/google"
 
 import { ReactNode } from "react"
@@ -10,6 +12,12 @@ type Props = {
 
 const fontMain = Roboto_Flex({ variable: "--font-main", subsets: ["latin"] })
 const fontMono = Roboto_Mono({ variable: "--font-mono", subsets: ["latin"] })
+
+export const viewport: Viewport = {
+    width: "device-width",
+    initialScale: 1,
+    minimumScale: 1
+}
 
 export default function Layout({ children }: Props) {
     const fontClassses = [fontMain.variable, fontMono.variable]
